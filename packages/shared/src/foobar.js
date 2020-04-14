5851d49d63ef54724bb3d1f760ec2d8fcfb12ede
Your Name <you@example.com>
2020-04-14 19:09:08 -0400

    build with tsc

---
+++ packages/shared/src/foobar.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+function foobar() {
+    return "asdffoo";
+}
+exports.foobar = foobar;
+//# sourceMappingURL=foobar.js.map
--- packages/shared/src/foobar.ts
+++ packages/shared/src/foobar.ts
@@ -1,3 +1,3 @@
 export function foobar() {
-  return "asdffoo";
+  return "asdf";
 }
