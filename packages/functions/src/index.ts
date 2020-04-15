7863111f65b8a87c7117b29391c41a7e38bc9fde
TJ Lavelle <tj@lavelle.io>
2020-04-15 12:07:56 -0400

    Sync

---
+++ packages/functions/src/index.ts
@@ -0,0 +1,8 @@
+// import { foobar } from "shared/foobar";
+import * as foobarGroup from "./foobar";
+
+export function doStuff() {
+  // return foobar();
+}
+
+export const foobar = foobarGroup;
--- packages/shared/src/foobar.ts
+++ packages/shared/src/foobar.ts
@@ -1,3 +1,3 @@
 export function foobar() {
-  return "asdfa";
+  return "foofoo";
 }
